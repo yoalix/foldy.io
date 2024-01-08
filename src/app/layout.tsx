b94dc40d1fbfdbce35459bff4b1151e4326f4c2e
Yoali <yoali.carachure@gmail.com>
2024-01-08 13:47:14 -0600

    made the nav bar mobile friendly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,33 @@
 import type { Metadata } from "next";
 import { Header } from "@/components/header";
 import { getAuthenticatedAppForUser } from "@/lib/firebase/firebase";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "@/components/query-provider";
+import { Body } from "@/components/body";
 
 export const dynamic = "force-dynamic";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Foldy.io",
-  description: "Currating the best of the web",
+    title: "Foldy.io",
+    description: "Currating the best of the web",
+    icons: { icon: "/favicon.ico" },
 };
 
 export default async function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  const { currentUser } = await getAuthenticatedAppForUser();
-  return (
-    <html lang="en">
-      <QueryProvider>
-        <Header initalUser={currentUser} />
-        <body className={inter.className + " ml-40 max-w-[680px]"}>
-          {children}
-        </body>
-      </QueryProvider>
-    </html>
-  );
+    const { currentUser } = await getAuthenticatedAppForUser();
+    return (
+        <html lang="en">
+            <QueryProvider>
+                <Header initalUser={currentUser} />
+                <Body >
+                    {children}
+                </Body>
+            </QueryProvider>
+        </html>
+    );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,20 +14,96 @@ import { Settings } from "@/components/icons/settings";
 import { Button } from "@/components/ui/button";
 import { useUserSession } from "@/hooks/firebase/useUserSession";
 import { FirebaseUser } from "@/lib/firebase/firebase";
+import { useBreakpoints } from "@/hooks/useBreakpoints";
+import { Menu } from "@/components/icons/menu";
 
 type HeaderProps = {
   initalUser?: FirebaseUser | null;
 };
 
+const NavMenuItems = [
+  { name: "Explore", href: "/explore", icon: <Compass /> },
+  { name: "Profile", href: "/profile", icon: <CircleUser /> },
+  { name: "Settings", href: "/settings", icon: <Settings /> },
+];
+
+const MobileNav = () => {
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollTop =
+        window.scrollY || document.documentElement.scrollTop;
+      if (currentScrollTop > lastScrollTop) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+      setLastScrollTop(currentScrollTop);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollTop]);
+  return (
+    <header>
+      <div
+        className={`fixed top-0 left-0 right-0 flex justify-between p-9 bg-[#FFF] transition-all duration-500 ${
+          isVisible
+            ? "translate-y-0 opacity-100"
+            : "-translate-y-full opacity-0"
+        }`}
+      >
+        <Link href="/" className="flex w-full items-center">
+          <img
+            className="text-2xl font-bold h-4 w-4"
+            src="/icons/logo.png"
+            alt="FoldyIcon"
+          />
+          <h1 className="text-primary font-bold italic">FOLDY</h1>
+        </Link>
+        <Link href="/settings">
+          <Button variant="ghost" size="icon">
+            <Menu />
+          </Button>
+        </Link>
+      </div>
+      <nav
+        className={`fixed bottom-3 left-0 right-0 flex justify-center transition-all duration-500 ${
+          isVisible ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
+        }`}
+      >
+        <Link href="/explore">
+          <Button
+            className="font-bold border-r-0 rounded-r-none rounded-l-3xl"
+            variant="outline"
+            size="lg"
+          >
+            Explore
+          </Button>
+        </Link>
+        <Link href="/profile">
+          <Button
+            className="border-l-0 rounded-l-none rounded-r-3xl"
+            variant="outline"
+            size="lg"
+          >
+            Profile
+          </Button>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
 export function Header({ initalUser }: HeaderProps) {
   const user = useUserSession(initalUser);
-  console.log({ user });
-  const navMenuItems = [
-    { name: "Explore", href: "/explore", icon: <Compass /> },
-    { name: "Profile", href: "/profile", icon: <CircleUser /> },
-    { name: "Settings", href: "/settings", icon: <Settings /> },
-  ];
-  return (
+  const breakpoint = useBreakpoints();
+  const isMobile = breakpoint === "sm" || breakpoint === "xs";
+
+  return isMobile ? (
+    <MobileNav />
+  ) : (
     <header className="fixed top-0 left-0 bottom-0 flex flex-col justify-around items-center  border-r border-black-50 ">
       <Link href="/" className="flex w-full items-center p-10 border-b">
         <img
@@ -37,9 +113,10 @@ export function Header({ initalUser }: HeaderProps) {
         />
         <h1 className="text-primary font-bold italic">FOLDY</h1>
       </Link>
+
       {user ? (
         <div className="flex flex-1 flex-col items-start gap-8 mt-8">
-          {navMenuItems.map((item) => (
+          {NavMenuItems.map((item) => (
             <Button
               key={`nav-${item.name}`}
               className={"text-sm mr-4"}
